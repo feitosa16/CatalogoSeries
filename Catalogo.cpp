6a77b9b5aab4aebeaf1d4aef8b7365a53ed5db8a
Heloisa Feitosa <103234249+feitosa16@users.noreply.github.com>
2024-05-24 22:19:36 +0000

    My second commit addition of functions for recovering, editing and deleting a record

--- Catalogo.cpp
+++ Catalogo.cpp
@@ -2,10 +2,42 @@
 #include <iostream>
 using namespace std;
 
+//Adiciona uma nova série ao catalogo
 void Catalogo::addSerie(const Serie& serie) {
     series.push_back(serie);
 }
+//Recupera uma serie pelo Id e retorna um ponteiro apra serie se encontrada
+Serie* Catalogo::getSerie(int id){
+    for (auto& serie : series){
+        if(serie.getId() == id){
+            return &serie;
+        }   
+        return nullptr;
+    }
+}
 
+// Edita uma série existente pelo ID, substituindo pelos novos dados da série, retorna true se a série foi encontrada e editada, caso contrário retorna false
+bool Catalogo::editSerie(int id, const Serie& novaSerie) {
+    for (auto& serie : series) {
+        if (serie.getId() == id) {
+            serie = novaSerie;
+            return true;
+        }
+    }
+    return false;
+}
+
+// Remove uma série pelo ID, retorna true se a série foi encontrada e removida, caso contrário retorna false
+bool Catalogo::removeSerie(int id) {
+    for (auto it = series.begin(); it != series.end(); ++it) {
+        if (it->getId() == id) {
+            series.erase(it);
+            return true;
+        }
+    }
+    return false;
+}
+//Exibe todas as series no catálogo 
 void Catalogo::displaySeries() const {
     for (const auto& serie : series) {
         serie.display();
--- Interface.cpp
+++ Interface.cpp
@@ -2,21 +2,27 @@
 #include <iostream>
 using namespace std;
 
+
+//Exibe menu principal
 void Interface::exibirMenuPrincipal() {
     cout << "Menu Principal" << endl;
     cout << "1. Séries" << endl;
     cout << "2. Sair" << endl;
     cout << "Escolha uma opção: ";
 }
-
+//Exibe o submenu da serie 
 void Interface::exibirSubmenuSeries() {
-    cout << "Submenu Séries" << endl;
+  cout << "Submenu Séries" << endl;
     cout << "1. Incluir um novo registro" << endl;
-    cout << "2. Exibir todas as séries" << endl;
-    cout << "3. Voltar ao menu principal" << endl;
+    cout << "2. Recuperar um registro" << endl;
+    cout << "3. Editar um registro" << endl;
+    cout << "4. Excluir um registro" << endl;
+    cout << "5. Exibir todas as séries" << endl;
+    cout << "6. Voltar ao menu principal" << endl;
     cout << "Escolha uma opção: ";
 }
 
+//Inclui uma nova serie no catalogo
 void Interface::incluirSerie() {
     string nome, atores, personagens, canal;
     int ano, temporadas, numEpisodios, nota;
@@ -46,8 +52,74 @@ void Interface::incluirSerie() {
     cout << "Série adicionada com sucesso!" << endl;
 }
 
+// Recupera uma série pelo ID
+void Interface::recuperarSerie() {
+    int id;
+    cout << "Digite o ID da série a ser recuperada: ";
+    cin >> id;
+
+    Serie* serie = catalogo.getSerie(id);
+    if (serie) {
+        serie->display();
+    } else {
+        cout << "Série não encontrada." << endl;
+    }
+}
+// Edita uma série pelo ID
+void Interface::editarSerie() {
+    int id, ano, temporadas, numEpisodios, nota;
+    string nome, atores, personagens, canal;
+
+    cout << "Digite o ID da série a ser editada: ";
+    cin >> id;
+
+    Serie* serie = catalogo.getSerie(id);
+    if (serie) {
+        cout << "Digite o novo nome: ";
+        cin.ignore();
+        getline(cin, nome);
+        cout << "Digite o novo ano de lançamento: ";
+        cin >> ano;
+        cout << "Digite o novo número de temporadas: ";
+        cin >> temporadas;
+        cout << "Digite o novo número de episódios: ";
+        cin >> numEpisodios;
+        cin.ignore();
+        cout << "Digite os novos principais atores/atrizes: ";
+        getline(cin, atores);
+        cout << "Digite os novos personagens principais: ";
+        getline(cin, personagens);
+        cout << "Digite o novo canal/streaming: ";
+        getline(cin, canal);
+        cout << "Digite a nova nota de classificação (0-10): ";
+        cin >> nota;
+
+        Serie novaSerie(id, nome, ano, temporadas, numEpisodios, atores, personagens, canal, nota);
+        if (catalogo.editSerie(id, novaSerie)) {
+            cout << "Série editada com sucesso!" << endl;
+        } else {
+            cout << "Erro ao editar a série." << endl;
+        }
+    } else {
+        cout << "Série não encontrada." << endl;
+    }
+}
+
+// Exclui uma série pelo ID
+void Interface::excluirSerie() {
+    int id;
+    cout << "Digite o ID da série a ser excluída: ";
+    cin >> id;
+
+    if (catalogo.removeSerie(id)) {
+        cout << "Série excluída com sucesso!" << endl;
+    } else {
+        cout << "Série não encontrada." << endl;
+    }
+}
+
 void Interface::iniciar() {
-    int opcao = 0;
+      int opcao = 0;
     while (opcao != 2) {
         exibirMenuPrincipal();
         cin >> opcao;
@@ -64,14 +136,23 @@ void Interface::iniciar() {
                     incluirSerie();
                     break;
                 case 2:
-                    catalogo.displaySeries();
+                    recuperarSerie();
                     break;
                 case 3:
+                    editarSerie();
+                    break;
+                case 4:
+                    excluirSerie();
+                    break;
+                case 5:
+                    catalogo.displaySeries();
+                    break;
+                case 6:
                     break;
                 default:
                     cout << "Opção inválida!" << endl;
                 }
-            } while (subopcao != 3);
+            } while (subopcao != 6);
             break;
         case 2:
             cout << "Saindo..." << endl;
